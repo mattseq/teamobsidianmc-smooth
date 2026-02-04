f17f30d64cb11e1832a4a9f62f36a549989baf6a
mattseq <matthewsequeir@gmail.com>
2026-02-03 20:41:39 -0600

    use canvas for images and preload them

--- app/components/PortalIntro.tsx
+++ app/components/PortalIntro.tsx
@@ -1,58 +1,73 @@
 "use client";
 
+import assert from 'assert';
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import { useEffect, useRef, useState } from 'react'
 
 gsap.registerPlugin(ScrollTrigger);
 
-const images = Array.from({ length: 31 }, (_, i) => 
+const frameCount = 31;
+
+const images = Array.from({ length: frameCount }, (_, i) => 
     `/portal_animation/${(i + 1).toString().padStart(4, '0')}.png`
 );
 
 export default function PortalIntro( { className }: { className?: string } ) {
-    const imgRef = useRef<HTMLImageElement>(null);
-    const [progress, setProgress] = useState(0);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
-        const image = imgRef.current;
-        if (!image) return;
+        let playhead = { frame: 0 };
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext('2d');
+        if (!canvas || !context) return;
 
+        let currentFrame = -1;
+        let imgs: HTMLImageElement[] = [];
 
-        const tween = gsap.to("#intro", {
+        // function to render the current frame based on playhead.frame
+        function render() {
+            const frame = Math.round(playhead.frame);
+            if (frame !== currentFrame && imgs[frame]?.complete) {
+                assert(canvas && context);
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.drawImage(imgs[frame], 0, 0);
+                currentFrame = frame;
+            }
+        }
+
+        // preload images
+        imgs = images.map((src, i) => {
+            const img = new Image();
+            img.src = src;
+            i || (img.onload = render);
+            console.log(`Preloading image: ${i}`);
+            return img;
+        });
+        
+        // animate playhead.frame through frames on scroll
+        gsap.to(playhead, {
+            frame: frameCount - 1,
+            ease: "none",
             scrollTrigger: {
                 trigger: "#intro",
                 start: "top top",
                 end: "top -200%",
                 scrub: true,
                 pin: true,
-                onUpdate: (self) => {
-                    setProgress(self.progress);
-                },
+                pinnedContainer: "#intro",
+                onUpdate: render,
+                markers: true,
             },
-        });
-
-        return () => {
-            tween.scrollTrigger?.kill();
-            tween.kill();
-        }
+        })
     }, []);
-
-    // if finished, don't render anything
-    if (progress >= 1) {
-        return null;
-    }
-
-    // choose current image based on progress
-    const index = Math.floor(progress * (images.length - 1));
-    const src = images[index];
-
     return (
-        <img
-            ref={imgRef}
-            src={src}
-            alt="Entering Nether Portal Animation"
+        <canvas 
             className={className}
+            id="portal-canvas"
+            ref={canvasRef}
+            width={1920}
+            height={950}
         />
     );
 }
